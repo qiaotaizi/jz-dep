51ae5803cc329061ad5961cae05e9d1f7258cdc4
姜志恒 <jiangzh96@chinaunicom.cn>
2021-12-22 15:18:49 +0800

    依赖继承结构定义

---
+++ jz-common/jz-util/src/main/java/com/jaiz/common/utils/DtUtil.java
@@ -0,0 +1,12 @@
+package com.jaiz.common.utils;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * 日期及时间工具类
+ */
+@UtilityClass
+public class DtUtil {
+
+    // TODO
+}
---
+++ jz-common/jz-util/src/test/java/com/jaiz/common/utils/DtUtilTest.java
@@ -0,0 +1,21 @@
+package com.jaiz.common.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("时间日期工具类单元测试")
+public class DtUtilTest {
+
+    @Test
+    @DisplayName("示例单元测试")
+    public void example(){
+        System.out.println("Hello");
+    }
+
+    @Test
+    @DisplayName("示例单元测试2")
+    public void example2(){
+        System.out.println("Hello world");
+    }
+
+}
